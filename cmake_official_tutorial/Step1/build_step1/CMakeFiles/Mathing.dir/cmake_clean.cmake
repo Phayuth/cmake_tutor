24ecac9d5b47a3ea383bd266b6ba19a7a100facb
yuth <yuth>
2022-11-05 08:16:13 -0400

    Initial Commit

---
+++ cmake_official_tutorial/Step1/build_step1/CMakeFiles/Mathing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mathing.dir/mathing.cpp.o"
+  "Mathing.pdb"
+  "Mathing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mathing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake_official_tutorial/Step1/build_step1/TutorialConfig.h
@@ -0,0 +1,6 @@
+// the configured options and settings for Tutorial
+// TODO 10: Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
+
+#define Tutorial_VERSION_MAJOR 7
+#define Tutorial_VERSION_MINOR 0
+#define Tutorial_CMAKE_CXX_STANDARD 11
